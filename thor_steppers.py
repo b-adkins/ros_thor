f7a585555cef6dfc85ffff19dfacf2850ad97ca5
Boone Adkins <boone.adkins@gmail.com>
2017-04-09 22:48:04 -0700

    Now supports two motors - enough to drive the differential art5 and art6 articulations.

--- thor_steppers.py
+++ thor_steppers.py
@@ -2,38 +2,58 @@
 
 ## Hello world! Tells A4988 to turn stepper motor at constant velocity.
 
+import sys
 import time
 
 import RPi.GPIO as GPIO
 
-# Set up pins
+#
+# Pin configuration
+#
+
+
 GPIO.setmode(GPIO.BOARD)
 
 # Enable
 GPIO.setup(5, GPIO.OUT, initial=True)
+GPIO.setup(10, GPIO.OUT, initial=True)
 
 # Step
 GPIO.setup(7, GPIO.OUT, initial=False)
+GPIO.setup(11, GPIO.OUT, initial=False)
 
 # Dir
-GPIO.setup(8, GPIO.OUT)
+GPIO.setup(8, GPIO.OUT, initial=False)
+GPIO.setup(12, GPIO.OUT, initial=True)
 
 def enable():
     '''
     Sets ENABLE pin so A4988s move
     '''
     GPIO.output(5, False)
+    GPIO.output(10, False)
 
 def disable():
     '''
     '''
     GPIO.output(5, True)
+    GPIO.output(10, True)
 
 
 enable()
 
+if len(sys.argv) > 1:
+    if sys.argv[1] == "yaw":
+        GPIO.output(8, True)
+        GPIO.output(12, True)
+    elif sys.argv[1] == "-yaw":
+        GPIO.output(8, False)
+        GPIO.output(12, False)
+
 while(True):
     GPIO.output(7, True)
+    GPIO.output(11, True)
     time.sleep(1e-5)
     GPIO.output(7, False)
-    time.sleep(1e-2)
+    GPIO.output(11, False)
+    time.sleep(3e-2)
