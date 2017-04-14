600d54d8a7d2a5b9813b0cadc178d0e98bfc9a1a
Boone Adkins <boone.adkins@gmail.com>
2017-04-13 21:53:24 -0700

    Object-oriented refactor.

--- thor_steppers.py
+++ thor_steppers.py
@@ -7,53 +7,82 @@ import time
 
 import RPi.GPIO as GPIO
 
+
+class Motor(object):
+    def __init__(self, direction, step, enable):
+        self._direction_pin = direction
+        self._step_pin = step
+        self._enable_pin = enable
+
+        self._direction = False
+        
+        GPIO.setup(self._direction_pin, GPIO.OUT, initial=self._direction)
+        GPIO.setup(self._step_pin, GPIO.OUT, initial=False)
+        GPIO.setup(self._enable_pin, GPIO.OUT, initial=True)        
+
+    def enable(self):
+        '''
+        Sets ENABLE pin so A4988s moves.
+        '''
+        GPIO.output(self._enable_pin, False)
+
+    def disable(self):
+        '''
+        Sets ENABLE pin so A4988s goes limp.
+        '''
+        GPIO.output(self._enable_pin, True)
+
+    @property
+    def direction(self):
+        return _direction
+
+    @direction.setter
+    def direction(self, direction):
+        self._direction = direction
+        GPIO.output(self._direction_pin, direction)
+
+    def step(self):
+        GPIO.output(self._step_pin, True)
+        time.sleep(1e-5)
+        GPIO.output(self._step_pin, False)
+        
+       
 #
 # Pin configuration
 #
 
-
 GPIO.setmode(GPIO.BOARD)
 
-# Enable
-GPIO.setup(5, GPIO.OUT, initial=True)
-GPIO.setup(10, GPIO.OUT, initial=True)
 
-# Step
-GPIO.setup(7, GPIO.OUT, initial=False)
-GPIO.setup(11, GPIO.OUT, initial=False)
-
-# Dir
-GPIO.setup(8, GPIO.OUT, initial=False)
-GPIO.setup(12, GPIO.OUT, initial=True)
-
-def enable():
-    '''
-    Sets ENABLE pin so A4988s move
-    '''
-    GPIO.output(5, False)
-    GPIO.output(10, False)
-
-def disable():
-    '''
-    '''
-    GPIO.output(5, True)
-    GPIO.output(10, True)
+motors = {
+    "art5": Motor(8, 7, 5),
+    "art6": Motor(12, 11, 10)
+}
 
 
-enable()
+# Simple default executable
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "yaw":
+            motors["art5"].direction = True
+            motors["art6"].direction = True
+        elif sys.argv[1] == "-yaw":
+            motors["art5"].direction = False
+            motors["art6"].direction = False
+        else:
+            motors["art5"].direction = True
+            motors["art6"].direction = False
 
-if len(sys.argv) > 1:
-    if sys.argv[1] == "yaw":
-        GPIO.output(8, True)
-        GPIO.output(12, True)
-    elif sys.argv[1] == "-yaw":
-        GPIO.output(8, False)
-        GPIO.output(12, False)
+    for m in motors.values():
+        m.enable()
 
-while(True):
-    GPIO.output(7, True)
-    GPIO.output(11, True)
-    time.sleep(1e-5)
-    GPIO.output(7, False)
-    GPIO.output(11, False)
-    time.sleep(3e-2)
+    try:
+        while(True):
+            for m in motors.values():
+                m.step()
+            time.sleep(3e-2)
+            
+    finally:
+        for m in motors.values():
+            m.disable()
+        GPIO.cleanup()
