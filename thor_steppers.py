281a31e3af641adba6153f21ced926d43aa2b499
Boone Adkins <boone.adkins@gmail.com>
2017-07-04 14:40:58 -0700

    Many more motors wired up. Improved help message.

--- thor_steppers.py
+++ thor_steppers.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
-## Hello world! Tells A4988 to turn stepper motor at constant velocity.
+## Crude high level stepper motor controllers
 
+import os.path
 import sys
 import time
 
@@ -56,10 +57,20 @@ GPIO.setmode(GPIO.BOARD)
 
 
 motors = {
+    "art1": Motor(36, 35, 33),
+    "art2a": Motor(32, 31, 29),
+    "art2b": Motor(24, 23, 26),
+    "art3": Motor(22, 21, 19),
     "art4": Motor(16, 15, 13),
     "art5": Motor(8, 7, 5),
     "art6": Motor(12, 11, 10)
 }
+
+def usage(exe_name):
+    print('''Usage: {} cmd
+
+Valid commands: yaw, -yaw, roll, -roll, art4, art3, art2
+    '''.format(exe_name))
 
 
 # Simple default executable
@@ -67,6 +78,9 @@ if __name__ == "__main__":
     cmd = ""
     if len(sys.argv) > 1:
         cmd = sys.argv[1]
+    else:
+        usage(os.path.basename(sys.argv[0]))
+        sys.exit(2)
 
 
     for m in motors.values():
@@ -93,9 +107,24 @@ if __name__ == "__main__":
         motors["art6"].direction = True
         motors["art5"].enable()
         motors["art6"].enable()
-    else:
+    elif cmd == "art4":
         motors["art4"].direction = True
         motors["art4"].enable()
+    elif cmd == "art3":
+        motors["art3"].direction = True
+        motors["art3"].enable()
+    elif cmd == "art2":
+        motors["art2a"].direction = True
+        motors["art2b"].direction = True
+        motors["art2a"].enable()
+        motors["art2b"].enable()
+    elif cmd == "art1":
+        motors["art1"].direction = True
+        motors["art1"].enable()
+    else:
+        usage(os.path.basename(sys.argv[0]))
+        sys.exit(2)
+    
             
     try:
         while(True):
